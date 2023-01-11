379321e8a1b838893bceb8de421da854857a1b6f
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-01-11 18:46:24 +0300

    Vue.JS - Konuları

--- 02-Vue-Uygulama/app.js
+++ 02-Vue-Uygulama/app.js
@@ -1,5 +1,24 @@
 const app = Vue.createApp({
-    template: '<h2>Bu ilk uygulamamız</h2>'
-})
+    template:
+        `
+        <h2>Başlık: {{baslik}}</h2>
+        <h3>İçerik: {{icerik}}</h3>
+        <p>Beğeni Sayısı: {{begeni}}</p>
+        <button v-on:click="begen()">BEĞEN</button>
+    `,
+    data() {
+        return {
+            baslik: 'Vue.JS',
+            icerik: 'Vue.JS Konuları',
+            begeni: 0
+        }
+    },
+    methods: {
+        begen() {
+            console.log("Beğenildi.");
+            this.begeni++;
+        }
+    }
+});
 
 app.mount('#app');
