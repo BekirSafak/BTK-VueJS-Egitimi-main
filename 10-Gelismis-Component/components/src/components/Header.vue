0f6c89c7996388ad75ddefb6c2642316f908c0c9
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-01-15 20:37:34 +0300

    advanced-component

---
+++ 10-Gelismis-Component/components/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template lang="">
+  <div>
+    <slot></slot>
+    <br><hr><br>
+    <input type="text" v-model="name">
+    <br><br>
+    <input type="text" v-model="surname">
+    <p>{{nameSurname}}</p>
+    <br><hr><br>
+    <input type="number" v-model="number1"> <br><br>
+    <input type="number" v-model="number2"> <br><br>
+    <input type="number" v-model="total">
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  data() {
+    return {
+      name: '',
+      surname: '',
+      number1: 0,
+      number2: 0,
+      total: 0
+    }
+  },
+
+  watch: {
+    // Buraya çektiğimiz datayı izleceyecek ve o data üzerinde bir değişiklik olduğu zaman metot içersindeki işleri yapmaya başlayacak.
+    number1(newValue, oldValue) {
+      console.log("Yeni Sayı: ", newValue, "Eski Sayı: ", oldValue)
+      this.total = parseInt(this.number1) + parseInt(this.number2)
+    },
+    // Buraya çektiğimiz datayı izleceyecek ve o data üzerinde bir değişiklik olduğu zaman metot içersindeki işleri yapmaya başlayacak.
+    number2(newValue, oldValue) {
+      console.log("Yeni Sayı: ", newValue, "Eski Sayı: ", oldValue)
+      this.total = parseInt(this.number1) + parseInt(this.number2)
+    },
+  },
+
+  computed: {
+    nameSurname() {
+      return this.name + ' ' + this.surname;
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
